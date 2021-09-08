d269711ec29dd596d44de91961c8d28ef4fb77b3
JiangXingRu <2994532784@qq.com>
2021-09-08 22:17:43 +0800

    V1.00.0001 1. Rolling Guidance Filter Demo

---
+++ RollingGuidanceFilter/main.cpp
@@ -0,0 +1,27 @@
+
+
+#include "RollingGuidanceFilter.h"
+
+int main(){
+
+	String name = "./imgs/image.png";
+
+	Mat img = imread(name);
+
+	if(img.empty()){
+		printf("No such file.\n");
+		getchar();
+		exit(1);
+	}
+
+	clock_t startTime = clock();
+	Mat res = RollingGuidanceFilter::filter(img,3,25.5,4);
+	printf("Elapsed Time: %d ms\n",clock()-startTime);
+
+	imshow("img",img);
+	imshow("res",res);
+	imwrite("./imgs/res.png", res);
+	waitKey();
+
+	return 0;
+}
