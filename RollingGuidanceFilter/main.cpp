cdbc4a3210cfdf63711bf0510f904206c8eff711
JiangXingRu <2994532784@qq.com>
2023-03-12 11:27:09 +0800

    V1.00.0006 1. change main function

--- RollingGuidanceFilter/main.cpp
+++ RollingGuidanceFilter/main.cpp
@@ -6,20 +6,25 @@ int main(int argc, char *argv[])
 
 	cv::Mat img = cv::imread(name);
 
-	if(img.empty()){
+	if(img.empty())
+	{
 		printf("No such file.\n");
 		getchar();
 		exit(1);
 	}
 
 	clock_t startTime = clock();
-	cv::Mat res = RollingGuidanceFilter::filter(img,3,25.5,4);
+	float sigma_s = 3.f;
+	float sigma_r = 25.5f;
+	int iteration = 4;
+	cv::Mat res = RollingGuidanceFilter::filter(img, sigma_s, sigma_r, iteration);
 	clock_t endTime = clock();
 	printf("Elapsed Time: %d ms\n", endTime - startTime);
 
 	cv::imshow("img",img);
 	cv::imshow("res",res);
 	cv::imwrite("./imgs/res.png", res);
+
 	cv::waitKey();
 
 	return 0;
